1a57f13627390c17d245d27dc9ed1018b59068c2
Gerrit Coetzer <75617224+GertLC@users.noreply.github.com>
2022-11-20 21:42:05 +0200

    serial to arduino

--- EngDes_Projek/Projek_v3.py
+++ EngDes_Projek/Projek_v3.py
@@ -5,7 +5,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from Class import *
 import time
-
+import serial
 
 
 # Connect to camera
@@ -166,6 +166,11 @@ def detect_ball(frame) :
             ball.area = ball_area
             #ball_img = crop_frame(gray_img, ball.outline)
             ball_img = cv2.circle(frame, ball.center, ball.radius, (0,255,0), 2)
+
+            serialcomm.write("run".encode('utf-8'))
+            #time.sleep(0.5)
+            serialcomm.close
+
             return ball_img
         else :
             return None
@@ -272,6 +277,9 @@ count = 0
 ball = Ball(None, None, None, None, None, False)
 blob = Blob(None, None, False)
 cap = connect_camera()
+serialcomm = serial.Serial('COM3', 9600)
+#serialcomm.timeout = 1
+
 while cap.isOpened():
     ret, frame = cap.read()
     frame_copy =  frame.copy()
@@ -288,6 +296,9 @@ while cap.isOpened():
         print("defect value: ", defect)
         #cv2.imshow('frame', test_img)
         if defect == 1:
+            serialcomm.write("def".encode('utf-8'))
+            #time.sleep(0.5)
+            serialcomm.close
             break
         else:
             cv2.imshow('frame', test_img)
